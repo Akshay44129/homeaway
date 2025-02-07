2466d6dfac19526a074e37a577cfeb0c17525628
Akshay44129 <akshaysakariya123@gmail.com>
2025-02-07 15:57:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,127 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+}
+console.log(process.env.SECRET);
+
+const express=require("express");
+const app=express();
+const mongoose =require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride=require("method-override");
+const ejsmate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const { listingSchema , reviewSchema}=require("./schema.js");
+const Review = require("./models/review.js");
+const review = require("./models/review.js");
+const session= require("express-session");
+const MongoStore = require('connect-mongo');
+const flash=require("connect-flash");
+const passport =require("passport");
+const LocalStratagy =require("passport-local");
+const User =require("./models/user.js");
+
+
+// const UserActivation=require("UserActivation");
+
+const listingRouter = require('./routes/listing.js');
+const reviewRouter=require("./routes/review.js");
+const userRouter =require("./routes/user.js");
+const user = require("./models/user.js");
+
+
+// const MONGO_URL="mongodb://127.0.0.1:27017/homeaway";
+
+const dburl=process.env.ATLASDB_URL;
+
+
+main().then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL);
+// }
+
+async function main(){
+    await mongoose.connect(dburl);
+}
+ 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsmate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+const store=MongoStore.create({
+    mongoUrl:dburl,
+    crypto:{
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 *3600 ,
+
+});
+ 
+
+store.on("error", ()=>{
+    console.log("error in mongo session store", err);
+});
+
+
+const sessionOption ={
+    store,
+    secret:process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+}
+
+app.use(session(sessionOption));
+app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratagy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next) =>{
+res.locals.success =req.flash("success");
+res.locals.error =req.flash("error");
+// console.log(res.locals.success);
+res.locals.currUser =req.user;
+next();
+});
+
+
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+
+
+app.all("*",(req,res,next)=>{
+next(new ExpressError(404,"page not found! "));
+});
+
+app.use((err,req,res,next)=>{
+  let { statusCode,message}=err;
+  res.render("error.ejs",{message});
+  // res.status(statusCode).send(message);
+  // res.send("somthing went wrong!");
+});
+
+app.listen(8080, ()=>{
+    console.log("server is running on port 8080");
+});
---
+++ controllers/users.js
@@ -0,0 +1,50 @@
+const User =require("../models/user");
+
+
+
+module.exports.renderSignupForm  =(req, res) => {
+    console.log("Signup route hit!");
+    res.render("users/signup");
+};
+
+module.exports.signup =async (req, res) => {
+    try {
+        console.log("Request Body:", req.body); 
+        let { username, email, Password } = req.body;
+        const newUser = new User({ email, username });
+        const registerUser = await User.register(newUser, Password);
+        console.log("Registered User:", registerUser);
+        req.login(registerUser, (err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success", "Welcome to HomeAway!");
+            res.redirect("/listings");
+        });    
+    } catch (err) {
+        console.error("Signup Error:", err);
+        req.flash("error", "Signup failed. Please try again.");
+        res.redirect("/signup");
+    }
+};
+
+module.exports.renderLoginForm= (req,res)=>{
+    res.render("users/login.ejs");
+};
+
+module.exports.login= async(req,res)=>{
+    //  res.send(" welcome to the homeaway");
+    req.flash("success", "welcome back to homeaway ! ")
+    let redirectUrl =res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout=(req, res, next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next (err);
+        }
+        req.flash("success","you are logged out");
+        res.redirect("/listings");
+    })
+};
